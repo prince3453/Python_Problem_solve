cb4da359b60ca7db8fabb4460ca802fe3861a05b
prince3453 <princeghoghari9@gmail.com>
2022-11-15 08:56:13 -0500

    rotation in both side
    depending on User

--- List/Rotate_number.py
+++ List/Rotate_number.py
@@ -3,8 +3,19 @@ def rotate(arr,d):
     lst = []
     lst = arry[d:]+arry[:d]
     return lst
+
+def rotate_l(arr,d):
+    n = len(arr)
+    lst = []
+    lst = arry[-d:]+arry[:-d]
+    return lst
     
 arry = input("Enter the array to rotate : ").split(',')
-d = int(input("Enter the position to rotate on left side : "))
-
-print(rotate(arry,d))
+d = input("Enter the position to rotate on left or right side Please Enter (L/R) : ").upper()
+c = int(input("Ente the rotational movement : "))
+if d == 'L':
+    print(rotate(arry,c))
+elif d == 'R':
+    print(rotate_l(arry,c))
+else:
+    print('Enter proper choice...')
